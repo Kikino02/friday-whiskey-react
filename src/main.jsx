31777b01615a11a1ce1c9230a369f8cf6a8876d0
Kiko Gajdoš <kiko.gajdos@gmail.com>
2024-09-28 16:07:54 +0200

    analytics

--- src/main.jsx
+++ src/main.jsx
@@ -1,6 +1,8 @@
 import React from "react";
 import { createRoot } from "react-dom/client";
 import { RouterProvider, createBrowserRouter } from "react-router-dom";
+import { inject } from "@vercel/analytics";
+inject();
 
 import "./index.css";
 import "./fontAwesome.js";
